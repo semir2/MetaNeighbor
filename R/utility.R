4e7496fb91a378651f14a1628678ad1e9b48ad8d
Stephan Fischer <stephischer@gmail.com>
2018-09-25 11:26:40 -0400

    Replace roxygen comments by usual comments

--- R/utility.R
+++ R/utility.R
@@ -1,7 +1,7 @@
 #' Contains a collection of utility functions
 #'
 
-#' Scale matrix such that all colums sum to 0 and have l2-norm of 1
+# Scale matrix such that all colums sum to 0 and have l2-norm of 1
 normalize_cols <- function(M, ranked = TRUE) {
   M <- as.matrix(M)
   if (ranked) {
@@ -10,15 +10,15 @@ normalize_cols <- function(M, ranked = TRUE) {
   return(normalize_cols_cpp(M))
 }
 
-#' Return binary matrix with position of elements of list_names within full_list
+# Return binary matrix with position of elements of list_names within full_list
 find_subsets <- function(full_list, list_names) {
   return(sapply(list_names, function(name) full_list == name))
 }
 
-#' Compute approximate neighbor voting without cell-cell network
-#'
-#' voter_id is a binary matrix indicating cell types of the voters.
-#' If left empty, cell types are assumed to be the column names of the voters.
+# Compute approximate neighbor voting without cell-cell network
+#
+# voter_id is a binary matrix indicating cell types of the voters.
+# If left empty, cell types are assumed to be the column names of the voters.
 compute_votes_without_network <- function(candidates, voters, voter_id = NULL) {
   if (is.null(voter_id)) {
     voter_id <- design_matrix(colnames(voters))
@@ -30,7 +30,7 @@ compute_votes_without_network <- function(candidates, voters, voter_id = NULL) {
          (c(crossprod(candidates, rowSums(voters))) + rep(ncol(voters), ncol(candidates)))
 }
 
-#' Build cell x cell correlation network from scaled matrices
+# Build cell x cell correlation network from scaled matrices
 build_network <- function(set_A, set_B, ranked = TRUE) {
   result <- crossprod(set_A, set_B)
   if (!ranked) { return(result) }
@@ -42,7 +42,7 @@ build_network <- function(set_A, set_B, ranked = TRUE) {
   return(result)
 }
 
-#' Rank data approximately (sampling-based, calling close values as ties)
+# Rank data approximately (sampling-based, calling close values as ties)
 pseudo_rank <- function(x, breaks = 1000, depth = 1000) {
   m <- min(x)
   M <- max(x)
@@ -58,10 +58,10 @@ pseudo_rank <- function(x, breaks = 1000, depth = 1000) {
   return(bins)
 }
 
-#' Compute neighbor voting from cell x cell correlation network
-#'
-#' voter_id is a cell x labels binary matrix indicating cell types
-#' If left empty, cell types are assumed to be the column names of the network.
+# Compute neighbor voting from cell x cell correlation network
+#
+# voter_id is a cell x labels binary matrix indicating cell types
+# If left empty, cell types are assumed to be the column names of the network.
 compute_votes_from_network <- function(network, voter_id = NULL) {
   if (is.null(voter_id)) {
     voter_id <- design_matrix(colnames(network))
@@ -69,10 +69,10 @@ compute_votes_from_network <- function(network, voter_id = NULL) {
   return(network %*% voter_id / rowSums(network))
 }
 
-#' Compute AUROCs based on neighbor voting and candidate identities
-#'
-#' candidate_id is a binary matrix indicating the cell type of candidates
-#' If left empty, cell types are assumed to be the row names of the vote matrix.
+# Compute AUROCs based on neighbor voting and candidate identities
+#
+# candidate_id is a binary matrix indicating the cell type of candidates
+# If left empty, cell types are assumed to be the row names of the vote matrix.
 compute_aurocs <- function(votes, candidate_id = NULL) {
   if (is.null(candidate_id)) {
     positives <- design_matrix(rownames(votes))
@@ -86,7 +86,7 @@ compute_aurocs <- function(votes, candidate_id = NULL) {
   return(result)
 }
 
-#' Transform a vector with cell_type labels into a binary matrix
+# Transform a vector with cell_type labels into a binary matrix
 design_matrix <- function(cell_type) {
   cell_type <- as.factor(cell_type)
   if (length(levels(cell_type)) > 1) {
@@ -98,7 +98,7 @@ design_matrix <- function(cell_type) {
   return(result)
 }
 
-#' Create a result matrix with one row and one column for each unique cell type
+# Create a result matrix with one row and one column for each unique cell type
 create_result_matrix <- function(cell_type) {
   unique_cell_type <- unique(cell_type)
   result <- matrix(0, nrow = length(unique_cell_type), ncol = length(unique_cell_type))
@@ -107,12 +107,12 @@ create_result_matrix <- function(cell_type) {
   return(result)
 }
 
-#' Return study id from a label in format 'study_id|cell_type'
+# Return study id from a label in format 'study_id|cell_type'
 get_study_id <- function(cluster_name) {
   return(sapply(strsplit(cluster_name, "\\|"), head, 1))
 }
 
-#' Return cell type from a label in format 'study_id|cell_type'
+# Return cell type from a label in format 'study_id|cell_type'
 get_cell_type <- function(cluster_name) {
   return(sapply(strsplit(cluster_name, "\\|"), tail, 1))
 }
